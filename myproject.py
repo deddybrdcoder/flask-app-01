e3658970781e1163d130e3fe416a81f11ebf9255
dedi triyadi <dedi.triyadi.barnawan@gmail.com>
2021-09-06 16:42:25 +0700

    set up CI/CD 12

--- myproject.py
+++ myproject.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:yellow'> Hello, Gunicorn!-v.5</h1>"
+    return "<h1 style='color:yellow'> Hello, Gunicorn!-v.6</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
